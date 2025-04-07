eb32aef59ecc12ede7d0e8fb43aed35b7c57ca64
Leapense <48317757+Leapense@users.noreply.github.com>
2025-04-07 19:08:23 +0900

    Modified prompt on zenity_askpass.sh

--- zenity_askpass.sh
+++ zenity_askpass.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zenity --password --title="Sudo Password Required" --text="Please enter your sudo password:" 2>/dev/null
+zenity --password --title="Sudo Password Required" --text="Please enter your sudo password to continue swap memory cleanup:" 2>/dev/null
 
